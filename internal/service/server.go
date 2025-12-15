ec462153857648af69444249c115e7395ad11cc1
Samir Faci <samir@es.net>
2025-12-15 13:31:21 -0500

    Switching to OpenAPI for HeathEndpoint
    Addresses #508

--- internal/service/server.go
+++ internal/service/server.go
@@ -4,16 +4,25 @@ import (
 	"log"
 )
 
+const (
+	SrvInfoDBKey               = "Database"
+	SrvInfoCommitKey           = "Commit"
+	SrvInfoVersionKey          = "Version"
+	SrvInfoEnterpriseCommitKey = "EnterpriseCommit"
+)
+
 // GetServerInfo returns basic Grafana Server info
 func (s *DashNGoImpl) GetServerInfo() map[string]any {
-	t, err := s.extended.Health()
+	response, err := s.GetClient().Health.GetHealth()
 	if err != nil {
 		log.Fatalf("Unable to get server health info, err: %v", err)
 	}
+	t := response.GetPayload()
 	result := make(map[string]any)
-	result["Database"] = t.Database
-	result["Commit"] = t.Commit
-	result["Version"] = t.Version
+	result[SrvInfoDBKey] = t.Database
+	result[SrvInfoCommitKey] = t.Commit
+	result[SrvInfoVersionKey] = t.Version
+	result[SrvInfoEnterpriseCommitKey] = t.EnterpriseCommit
 
 	return result
 }
---
+++ test/server_info_test.go
@@ -0,0 +1,47 @@
+package test
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/esnet/gdg/internal/config"
+	"github.com/esnet/gdg/internal/service"
+	"github.com/esnet/gdg/pkg/test_tooling"
+	"github.com/esnet/gdg/pkg/test_tooling/common"
+	"github.com/esnet/gdg/pkg/test_tooling/path"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServerInfo(t *testing.T) {
+	assert.NoError(t, os.Setenv("GDG_CONTEXT_NAME", common.TestContextName))
+
+	assert.NoError(t, path.FixTestDir("test", ".."))
+	config.InitGdgConfig(common.DefaultTestConfig)
+	var r *test_tooling.InitContainerResult
+	err := Retry(context.Background(), DefaultRetryAttempts, func() error {
+		r = test_tooling.InitTest(t, service.DefaultConfigProvider, nil)
+		return r.Err
+	})
+	assert.NotNil(t, r)
+	assert.NoError(t, err)
+	defer func() {
+		cleanupErr := r.CleanUp()
+		if cleanupErr != nil {
+			slog.Warn("Unable to clean up after test", "test", t.Name())
+		}
+	}()
+	apiClient := r.ApiClient
+	assert := assert.New(t)
+	res := apiClient.GetServerInfo()
+	assert.NotNil(res)
+	assert.NotEmpty(res[service.SrvInfoDBKey])
+	assert.NotEmpty(res[service.SrvInfoVersionKey])
+	assert.NotEmpty(res[service.SrvInfoCommitKey])
+	if apiClient.IsEnterprise() {
+		assert.NotEmpty(res[service.SrvInfoEnterpriseCommitKey])
+	} else {
+		assert.Empty(res[service.SrvInfoEnterpriseCommitKey])
+	}
+}
