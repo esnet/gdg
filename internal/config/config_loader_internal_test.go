114bcda08b456d23ee120a00de0bd2646b101072
Pavel Denisov <156642340+PavelsDenisovs@users.noreply.github.com>
2025-12-15 16:21:26 +0200

    Refactor: Simplify buildConfigSearchPath (#497)
    * refactor(internal/config): simplify buildConfigSearchPath with no logic changes
    
    * chore(internal/config): fix extra spaces and improve readibility of error message
    
    * refactor(internal/config): clarify buildConfigSearchPath return values
    
    * test(internal/config): add the tests for buildConfigSearchPath
    
    * fix(internal/config): correct multiple logic issues in buildConfigSearchPath
    
    * test(internal/config): add more test cases and update existing tests

---
+++ internal/config/config_loader_internal_test.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfigSearchPathBuilding(t *testing.T) {
+	assert := assert.New(t)
+	t.Run("with config file path and yml extension", func(t *testing.T) {
+		configDirs, configName, ext := buildConfigSearchPath("/something/config/importer.yml")
+		expectedConfigDirs := append(configSearchPaths, "/something/config")
+		assert.Equal(expectedConfigDirs, configDirs)
+		assert.Equal("importer", configName)
+		assert.Equal("yml", ext)
+	})
+
+	t.Run("with config file path and json extension", func(t *testing.T) {
+		configDirs, configName, ext := buildConfigSearchPath("/internal/config/templates.json")
+		expectedConfigDirs := append(configSearchPaths, "/internal/config")
+		assert.Equal(expectedConfigDirs, configDirs)
+		assert.Equal("templates", configName)
+		assert.Equal("json", ext)
+	})
+
+	t.Run("with config file without directory path", func(t *testing.T) {
+		configDirs, configName, ext := buildConfigSearchPath("config.yml")
+		assert.Equal(configSearchPaths, configDirs)
+		assert.Equal("config", configName)
+		assert.Equal("yml", ext)
+	})
+	
+	t.Run("empty input", func(t *testing.T) {
+		configDirs, configName, ext := buildConfigSearchPath("")
+		assert.Equal(configSearchPaths, configDirs)
+		assert.Equal("", configName)
+		assert.Equal("", ext)
+	})
+
+	t.Run("with config file path without extension", func(t *testing.T) {
+		configDirs, configName, ext := buildConfigSearchPath("/testing/config/importer")
+		expectedConfigDirs := append(configSearchPaths, "/testing/config")
+		assert.Equal(expectedConfigDirs, configDirs)
+		assert.Equal("importer", configName)
+		assert.Equal("", ext)
+	})
+}
