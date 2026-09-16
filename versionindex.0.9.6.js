a6eb719b278736b63f58d2d28fa901b0ca71e644
safaci2000 <safaci2000@users.noreply.github.com>
2026-09-16 18:09:44 +0000

    deploy: 9071a5cf4424cf614037f1a31e10e01799b5171c

---
+++ versionindex.0.9.6.js
@@ -0,0 +1,17 @@
+var relearn_versionindex = [
+  {
+    "baseURL": "/gdg",
+    "identifier": "current",
+    "isAbs": false,
+    "isLatest": true,
+    "title": "Devel",
+    "value": "current"
+  },
+  {
+    "baseURL": "/gdg/0.9.6",
+    "identifier": "0.9.6",
+    "isAbs": false,
+    "title": "v0.9.6",
+    "value": "096"
+  }
+]
